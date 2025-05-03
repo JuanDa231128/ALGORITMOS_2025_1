5fd57b7346824234f121adeab92a04644faec8ec
JuanDa231128 <jdmedinasarmiento@ucompensar.edu.co>
2025-05-02 22:05:15 -0500

    Metodo sin retorno (Clasificación mayor o menor de edad con bucle While )
    Desarrollar un programa que pida ingresar edades y clasifique cada edad como "Menor de 
    edad" o "Mayor de edad". El usuario puede finalizar escribiendo 0. Se deben usar 
    condicionales, un ciclo y métodos sin argumentos ni retorno.

---
+++ MOM3/metodoMenMayEdad.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class metodoMenMayEdad {
+    static Scanner scanner = new Scanner(System.in);
+    static int mayedad = 0;
+    static int menedad = 0;
+    static int edad = 1;
+    
+    public static void main(String[] args) {
+        instrucciones();
+        clasifiEdad();
+        impriResultados();
+    }
+public static void instrucciones() {
+        System.out.println("Ingrese la edad que deseas clasificar. Para finalizar ingresa 0.");
+    }
+public static void clasifiEdad() {     
+        while (edad != 0) {
+            System.out.println("Ingresa la edad:");
+            edad = scanner.nextInt();
+            if (edad > 0){
+                if (edad > 18){
+                    mayedad ++;
+                }else {
+                    menedad ++;
+                }   
+            }else if (edad == 0){
+                System.out.println("Programa terminado");
+            }
+        }    
+    }
+public static void impriResultados(){
+    System.out.println(mayedad + " personas son mayores de edad y " + menedad + " personas son menores de edad");
+}
+}
+
